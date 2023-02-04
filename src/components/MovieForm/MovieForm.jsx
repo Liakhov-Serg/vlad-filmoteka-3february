d990a0ef8ce13fa6abb4fec3209ee54c103bef24
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-02-04 14:27:18 +0200

    Update MovieForm.jsx
    14-27

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,8 +1,60 @@
-const MovieForm = () => {
-    return (
-        <div>MovieForm</div>
+import { Component } from "react";
 
-    )
+class MovieForm extends Component {
+    state = {
+        filmName: "",
+        url: "",
+        ganre: "",
+        views: ""
+    };
+
+    handleChangeInfo = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value});
+    };
+
+    render() {
+        const { filmName, url, ganre, views } = this.state;
+        return (
+            // <div>MovieForm</div>
+            <form>
+                <input
+                    name="filmName"
+                    value={filmName}
+                    onChange={this.handleChangeInfo}
+                    type="text"
+                    placeholder="Film name"
+                    required
+                    minLength={3} />
+                <input
+                    name="url"
+                    value={url}
+                    onChange={this.handleChangeInfo}
+                    type="text"
+                    placeholder="Avatar URL"
+                    required
+                    minLength={10} />
+                <select name="ganre"
+                    value={ganre}
+                    onChange={this.handleChangeInfo}>
+                    <option value="" disabled>
+                        CHoose ganre
+                    </option>
+                    <option value="horror">horror</option>
+                    <option value="comedy">comedy</option>
+                    <option value="drama">drama</option>
+                    <option value="fantasy">fantasy</option>
+                </select>
+                <input
+                    name="views"
+                    value={views}
+                    onChange={this.handleChangeInfo}
+                    placeholder="views"
+                    required
+                    type="number" />
+            </form>
+
+        );
 }
-
+}
 export default MovieForm;
